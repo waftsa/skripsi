95a343fca82be68b02a00ee41217ceb95140ebab
Wafa <72697854+waftsa@users.noreply.github.com>
2025-07-28 16:49:37 +0700

    Update main.py

--- Program/WEBSITE/main.py
+++ Program/WEBSITE/main.py
@@ -1,3 +1,5 @@
 import streamlit as st
 
 st.title('ANALISIS SENTIMEN TERHADAP ISU GENOSIDA ISRAEL KEPADA PALESTINA')
+
+st.write('Aplikasi ini merupakan hasil dari penelitian skripsi penulis yang berjudul Analisis Sentimen ') 
