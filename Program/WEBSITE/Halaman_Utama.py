41b4c507f7a4043c10cd8fe4f61aa1c5f1c78f6f
Wafa <72697854+waftsa@users.noreply.github.com>
2026-01-31 17:29:09 +0700

    Revisi Seminar

--- Program/WEBSITE/Halaman_Utama.py
+++ Program/WEBSITE/Halaman_Utama.py
@@ -19,37 +19,20 @@ st.markdown("---")
 
 st.subheader("👨‍💻 Tim Pengembang")
 
-BASE_DIR = Path(__file__).resolve().parent
-image_developer = BASE_DIR / "image" / "wafa.jpg"
-image_dosbim = BASE_DIR / "image" / "Bu Helen.jpg"
-image_dosbim2 = BASE_DIR / "image" / "Bu Mira.jpg"
-
-
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    # if image_developer.exists():
-    #     st.image(str(image_developer), width=100)
         st.write("**Wafa Tsabita**")
         st.write("*Developer*")
-    # else:
-    #     st.error(f"Gambar tidak ditemukan: {image_developer}")
 
 with col2:
-    # if image_dosbim.exists():
-    #     st.image(str(image_dosbim), width=100)
         st.write("**Dr. Afrida Helen, S.T., M.Kom.**")
         st.write("*Pembimbing Utama*")
-    # else:
-    #     st.error(f"Gambar tidak ditemukan: {image_dosbim}")
 
 with col3:
-    # if image_dosbim2.exists():
-    #     st.image(str(image_dosbim2), width=100)
         st.write("**Dr. Mira Suryani, S.Pd, M.Kom.**")
         st.write("*Co-Pembimbing*")
-    # else:
-    #     st.error(f"Gambar tidak ditemukan: {image_dosbim2}")
+
 
 st.markdown("---")
 
